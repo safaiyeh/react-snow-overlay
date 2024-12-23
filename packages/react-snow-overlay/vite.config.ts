3e3dce7308478bc43a2fa30e2b8fbb5fddb5722d
Code Cowboy / Matthew Williams <matthew@magiceden.io>
2024-12-23 17:26:47 +1000

    Preserve 'use client' directive using rollup plugin

--- packages/react-snow-overlay/vite.config.ts
+++ packages/react-snow-overlay/vite.config.ts
@@ -2,6 +2,7 @@ import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import dts from 'vite-plugin-dts';
+import preserveDirectives from 'rollup-preserve-directives';
 
 export default defineConfig({
   build: {
@@ -29,5 +30,6 @@ export default defineConfig({
     dts({
       rollupTypes: true,
     }),
+    preserveDirectives(),
   ],
 });
